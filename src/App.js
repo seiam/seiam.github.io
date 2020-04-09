1bf33f52d744f4cd1471105a93a6397b3c4104c0
seiam <seiamsalehi@gmail.com>
2020-04-09 18:58:14 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavigationBar from './components/NavigationBar'
+import './App.css';
+
+function App() {
+
+  document.title = "Hello bb"
+  return (
+    <div className="App">
+    	<NavigationBar/>
+    </div>
+  );
+}
+
+export default App;
