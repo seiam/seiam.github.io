04a0d47cebaca8e143044d47800a7f3e47004913
seiam <seiamsalehi@gmail.com>
2020-04-09 19:32:06 -0400

    Updates

---
+++ precache-manifest.540a79286652b92eb635d6efe591927f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7220ffdd58247824ed13fd75aa83cb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14b72b7057a6c707c3b1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2f08cd585c0194e287e8",
+    "url": "/static/js/2.5e7123c4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.5e7123c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14b72b7057a6c707c3b1",
+    "url": "/static/js/main.3420f515.chunk.js"
+  },
+  {
+    "revision": "0f7e17235ce2106ccee1",
+    "url": "/static/js/runtime-main.549be430.js"
+  }
+]);
